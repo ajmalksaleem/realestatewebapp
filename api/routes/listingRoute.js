53e17307f6a2ce937b7aa83969d04b8e81a59af1
Ajmal k Saleem <ajmalksaleem@gmail.com>
2023-12-30 13:23:05 +0530

    major change

--- api/routes/listingRoute.js
+++ api/routes/listingRoute.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createListing, deleteListing } from "../controllers/listing-controller.js";
+import { createListing, deleteListing, updateListing } from "../controllers/listing-controller.js";
 import { verifyUser } from "../utils/verifyUser.js";
 const listingRouter = Router();
 
 listingRouter.post('/create', verifyUser, createListing)
 listingRouter.delete('/delete/:id', verifyUser, deleteListing)
+listingRouter.put('/update/:id', verifyUser, updateListing)
 
 export default listingRouter
